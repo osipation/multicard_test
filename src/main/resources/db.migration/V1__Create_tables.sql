a82f754ff87d316a9224a0aa37450aeaeb70f995
osipation <nik.79567@gmail.com>
2022-12-13 18:37:27 +0400

    добавил таблицы покупок и информации о них

---
+++ src/main/resources/db.migration/V1__Create_tables.sql
@@ -0,0 +1,19 @@
+CREATE TABLE "purchases" (
+    "id" int,
+    "name" text not null,
+    CONSTRAINT "pk_purchase_id" PRIMARY KEY(id)
+);
+
+CREATE TABLE "purchase_info" (
+    "id" int,
+    "name" text not null,
+	"lastname" text not null,
+	"age" int,
+	"purchase_item" int not null,
+	"count" int not null,
+	"amount" numeric not null check (amount > 0),
+	"purchase_date" timestamp not null,
+    CONSTRAINT "pk_purchase_info_id" PRIMARY KEY(id),
+	CONSTRAINT "fk_purchase_info_purchase_item_id" FOREIGN KEY("purchase_item")
+    	REFERENCES "purchases" ("id")
+);
