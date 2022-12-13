5506d0eed985be0281e16be6b56372bcd00ba20b
osipation <nik.79567@gmail.com>
2022-12-13 14:54:04 +0400

    init commit

---
+++ src/main/java/com/osipation/multicard_test/MulticardTestApplication.java
@@ -0,0 +1,13 @@
+package com.osipation.multicard_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulticardTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulticardTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osipation/multicard_test/MulticardTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osipation.multicard_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulticardTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
