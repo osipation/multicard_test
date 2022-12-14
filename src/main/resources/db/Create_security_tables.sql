98ab44daf7e90a35bcf2d709c50c70c6dbaffa4b
osipation <nik.79567@gmail.com>
2022-12-14 18:21:35 +0400

    добавил в user - name, lastname, age

--- src/main/resources/db/Create_security_tables.sql
+++ src/main/resources/db/Create_security_tables.sql
@@ -3,13 +3,17 @@ CREATE TABLE users
     id       bigint UNIQUE  not null,
     username text UNIQUE NOT NULL,
     password text        NOT NULL,
+    name text not null,
+    lastname text not null,
+    age int not null check(age > 0),
     enabled  boolean     NOT NULL,
     PRIMARY KEY (id)
 );
 
 INSERT INTO users
-VALUES (1, 'user1', '{noop}123', true),
-       (2, 'user2', '{noop}123', true);
+VALUES (1, 'user1', '{noop}123', 'Nik', 'Osipatov', 27, true),
+       (2, 'user2', '{noop}123', 'Roger', 'Federer', 39, true),
+	   (3, 'user3', '{noop}123', 'Mike', 'Wazovski', 18, true);
 
 CREATE TABLE authorities
 (
